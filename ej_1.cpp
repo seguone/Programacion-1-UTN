89c8639cae655f759696263b49208b691807e6b4
seguone <seguracristian_06@hotmail.com>
2025-04-07 16:03:11 -0300

    Primer Ejercicio del TP 1

---
+++ ej_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int n;
+
+    cout << "Ingrese un numero: ";
+    cin >>n;
+
+    cout << "Su numero es: " << n << endl;
+
+    return 0;
+}
