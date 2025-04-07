ab5d19d2ce7a909abae9f7dc0be6b82c0f3da0a0
seguone <seguracristian_06@hotmail.com>
2025-04-07 17:44:13 -0300

    Ejercicio 2 modificado TP 1

--- ej_2.cpp
+++ ej_2.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 main(){
-    int n1, n2;
+    int n1, n2,n3;
     int r;
     cout << "Ingrese un numero: ";
     cin >> n1;
     cout << "Ingrese otro numero: ";
     cin >> n2;
 
-    r=n1+n2;
+    n3=n1;
+    n1=n2;
+    n2=n3;
 
-    cout << "La suma de los 2 numero es: " << r << endl;
+    cout << n1 << endl;
+    cout << n2 << endl;
 
 
     return 0;
