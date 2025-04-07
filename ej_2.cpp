145a515f2f973108486f2fb93d161af5f7ee29aa
seguone <seguracristian_06@hotmail.com>
2025-04-07 16:09:55 -0300

    Segundo ejercicio del TP 1

---
+++ ej_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int n1, n2;
+    int r;
+    cout << "Ingrese un numero: ";
+    cin >> n1;
+    cout << "Ingrese otro numero: ";
+    cin >> n2;
+
+    r=n1+n2;
+
+    cout << "La suma de los 2 numero es: " << r << endl;
+
+
+    return 0;
+}
