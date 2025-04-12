9fc58fc068e82a71554a62c61083f0a7df48a7f2
seguone <seguracristian_06@hotmail.com>
2025-04-12 02:32:18 -0300

    Septimo ejercicio completado TP 1

--- ej_7.cpp
+++ ej_7.cpp
@@ -2,30 +2,17 @@
 using namespace std;
 
 main(){
-    int s1, s2, s3, s4, t;
-    float pro, ps1, ps2, ps3, ps4;
+    float imp, d, t;
 
-    cout << "Ingrese monton recaudado en la 1°semana: ";
-    cin >> s1;
-    cout << "Ingrese monton recaudado en la 2°semana: ";
-    cin >> s2;
-    cout << "Ingrese monton recaudado en la 3°semana: ";
-    cin >> s3;
-    cout << "Ingrese monton recaudado en la 3°semana: ";
-    cin >> s4;
+    cout << "Ingrese el importe del producto: ";
+    cin >> imp;
+    cout << "Ingrese el porcentaje a descontar: ";
+    cin >> d;
 
-    t = s1+s2+s3+s4;
-    pro= t/4;
-    ps1 = (float)s1*100/t;
-    ps2 = (float)s2*100/t;
-    ps3 = (float)s3*100/t;
-    ps4 = (float)s4*100/t;
+    t=imp-(d*imp/100);
 
-    cout << "El promedio recaudado de las 4 semanas es: " << pro << "%" << endl;
-    cout << "El porcentaje del monton recaudado en la 1°semana es: " << ps1 << "%" << endl;
-    cout << "El porcentaje del monton recaudado en la 2°semana es: " << ps2 << "%" << endl;
-    cout << "El porcentaje del monton recaudado en la 3°semana es: " << ps3 << "%" << endl;
-    cout << "El porcentaje del monton recaudado en la 4°semana es: " << ps4 << "%" << endl;
+    cout << "El importe a pagar con el descuento incluido es: " << t << "$" << endl;
+
 
     return 0;
 }
