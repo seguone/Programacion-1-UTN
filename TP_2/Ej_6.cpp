3699cd94f2c8d38e41caa68fdccb02936a323881
seguone <seguracristian_06@hotmail.com>
2025-04-15 09:04:29 -0300

    Sexto ejercicio completo TP 2

---
+++ TP_2/Ej_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int n1, n2, n3;
+
+    cout << "Ingrese un numero: ";
+    cin >> n1;
+    cout << "Ingrese un numero: ";
+    cin >> n2;
+    cout << "Ingrese un numero: ";
+    cin >> n3;
+
+    if(n1==n2 and n2==n3){
+        cout << "TODOS LOS NUMEROS SON IGUALES";
+    }
+
+    return 0;
+}
