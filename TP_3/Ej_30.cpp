2068d21acca9ff8c59d5a98c340a32bc52cbb7ba
seguone <seguracristian_06@hotmail.com>
2025-04-29 11:22:58 -0300

    Treintaavo ejercicio completo TP 3

--- TP_3/Ej_30.cpp
+++ TP_3/Ej_30.cpp
@@ -2,55 +2,34 @@
 using namespace std;
 
 main(){
-    int numPartido, minJugados, tarAmarilla, tarRoja, goles;
-    int no_jugado=0, jugados=0, tarjeta=0;
-    float proTarjetas;
-    int maxiGoles, maxiNum;
-    int racha_actual = 0, racha_maxima = 0;
+    int num, ant,div;
+    bool b=false, desordenado=false;
 
-    for(int i=1; i<=5; i++){
-        cout << "****NUEVO PARTIDO*****"<< endl;
-        cout << "Ingrese numero de partido: ";
-        cin >> numPartido;
-        cout << "Ingrese minutos juegados: ";
-        cin >> minJugados;
-        cout << "Ingrese cantidad de tarjetas amarillas: ";
-        cin >> tarAmarilla;
-        cout << "Ingrese cantidad de tarjetas rojas: ";
-        cin >> tarRoja;
-        cout << "Ingrese cantidad de goles: ";
-        cin >> goles;
+    for(int i=1; i<=7; i++){
+        cout << "Ingrese un numero: ";
+        cin >> num;
 
-        if(minJugados==0){
-            no_jugado++;
-        }
-        if(minJugados>=90){
-            jugados++;
-        }
-        if(tarAmarilla!=0 || tarRoja!=0){
-            tarjeta+=tarAmarilla;
-            tarjeta+=tarRoja;
-        }
-        if(i==1 || goles>maxiGoles){
-            maxiGoles=goles;
-            maxiNum=numPartido;
-        }
-        if (goles > 0) {
-            racha_actual++;
-            if (racha_actual > racha_maxima) {
-                racha_maxima = racha_actual;
+        div=0;
+        for(int x=1; x<=num; x++){
+            if(num%x==0){
+                div++;
             }
-        } else {
-            racha_actual = 0;
+        }
+        if(div==2){
+            if(b==false || num>=ant){
+                ant=num;
+                b=true;
+            }
+            else{
+                desordenado= true;
+            }
         }
     }
-    proTarjetas=tarjeta/5;
-    cout << "CANTIDAD DE PARTIDOS NO JUGADOS: " << no_jugado << endl;
-    cout << "CANTIDAD DE PARTIDOS JUGADOS COMPLETOS: " << jugados << endl;
-    cout << "PROMEDIO DE TARJETAS POR PARTIDO: " << proTarjetas << endl;
-    cout << "NUM DEL PARTIDO CON MAYOR CANTIDAD DE GOLES: " << maxiNum << endl;
-    cout << "CON GOLES: " << maxiGoles << endl;
-    cout << "MEJOR RACHA DE PARTIDOS CONVIRTIENDO GOLES: " << racha_maxima << " partidos consecutivos" << endl;
-
+    if (desordenado){
+        cout << "ESTA DESORDENADO" << endl;
+    }
+    else{
+        cout << "ESTA ORDENADO" << endl;
+    }
     return 0;
 }
