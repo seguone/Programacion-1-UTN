0f6dbb2111158db1ab70a62f60a6ca50459618c6
seguone <seguracristian_06@hotmail.com>
2025-05-03 21:03:08 -0300

    Cuarto ejercicio completo TP 4

---
+++ TP_4/Ej_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int i=1;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    while(i<=num){
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
