6ea430e18bfa8bca0af3ff0d997d8101a341526a
seguone <seguracristian_06@hotmail.com>
2025-05-03 21:06:07 -0300

    cuarto ejercicio completo TP 4

--- TP_4/Ej_4.cpp
+++ TP_4/Ej_4.cpp
@@ -2,13 +2,25 @@
 using namespace std;
 
 int main(){
-    int num;
-    int i=1;
+    int num1, num2;
+    int menor, mayor;
+
     cout << "Ingrese un numero: ";
-    cin >> num;
-    while(i<=num){
-        cout << i << endl;
-        i++;
+    cin >> num1;
+    cout << "Ingrese un numero: ";
+    cin >> num2;
+
+    if(num1>num2){
+        mayor=num1;
+        menor=num2;
+    }
+    else{
+        mayor=num2;
+        menor=num1;
+    }
+    while(menor<=mayor){
+        cout << menor << endl;
+        menor++;
     }
     return 0;
 }
